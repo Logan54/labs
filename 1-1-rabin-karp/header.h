9c343aedca542232e55f044bc43598b6a3de5efe
Logan54 <54l0gan@gmail.com>
2012-05-07 08:56:47 +0700

    Added 1-1-rabin-karp

--- 1-1-rabin-karp/header.h
+++ 1-1-rabin-karp/header.h
@@ -1,11 +1,6 @@
 #define SIZE_PATTERN 17
 #define SIZE_TMP 257 //Необходима степень двойки
 
-int hash (char sub_str[SIZE_PATTERN]);
-rehash (int old_hash, char old_ch, char new_ch, int lenpat);
-int seek_substring_RK (struct pattern * PAT);
-void rewrit (struct circle_buf * BUF, char sub_tmp[SIZE_TMP]);
-
 struct pattern {
 	char value[SIZE_PATTERN];
 	int length;
@@ -20,3 +15,12 @@ struct circle_buf {
 	int rec;
 	char buf[SIZE_TMP]; 
 };
+
+int hash (char sub_str[SIZE_PATTERN]);
+int rehash (int old_hash, char old_ch, char new_ch, int lenpat);
+int seek_substring_RK (struct pattern * PAT);
+void rewrit (struct circle_buf * BUF, char sub_tmp[SIZE_TMP]);
+
+
+
+
